3ba67d7bacf22c10ff57611c96d46fd9ba1f1ff3
Андрій Мірошник <mirik23452@gmail.com>
2023-07-13 14:13:53 +0300

    SimpleMoving

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    [SerializeField] private PlayerController _playerController;
+    [SerializeField] private Camera _camera;
+    private Vector3 _lastPos;
+    private Ray _touchRay => _camera.ScreenPointToRay(Input.mousePosition);
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _playerController.MoveTo(GetRayPosition(_touchRay));
+        }
+    }
+    public Vector3 GetRayPosition(Ray ray)
+    {
+        RaycastHit hit;
+        Vector3 pos=Vector3.zero;
+        
+        if (Physics.Raycast(ray, out hit,float.MaxValue,1))
+        {
+            int x = (int)(hit.point.x);
+            int z = (int)(hit.point.z);
+            pos = new Vector3(x, 0, z);
+        }
+
+        _lastPos = hit.point;
+        return pos;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Vector3 position = _lastPos;
+        position.y += 0.01f;
+        Gizmos.DrawWireSphere(position, 0.3f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Vector3 _previousPosition;
+    private Vector3 _newPosition;
+    private bool _moving;
+    private float _movingProgress=3;
+    private float _progressFactor=0;
+    private Vector3 _yPos=new Vector3(0,1f,0);
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    public void MoveTo(Vector3 position)
+    {
+        _previousPosition = _transform.position;
+        _newPosition = position+_yPos;
+        _movingProgress = 0;
+        _progressFactor = speed / Vector3.Distance(_previousPosition, _newPosition);
+        _moving = true;
+    }
+
+    public Vector3 GetPos()
+    {
+        return _transform.position;
+    }
+    private void Update()
+    {
+        if (_moving)
+        {
+            _transform.position = Vector3.Lerp(_previousPosition, _newPosition,  _movingProgress);
+            _movingProgress += Time.deltaTime * speed;
+
+            if (_movingProgress >= 1)
+            {            
+                _movingProgress-=1;
+                _moving = false;
+            }
+        }
+    }
+}
